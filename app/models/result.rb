24d3e33536e3a0c772508f1982f658e71868479f
Brenton Annan <brentonannan@brentonannan.com>
2021-11-26 11:39:23 +1100

    Update result marks by choosing the higher mark

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,12 @@
 class Result < ApplicationRecord
+  after_initialize do
+    self.obtained_mark ||= 0
+    self.available_marks ||= 0
+  end
+
+  def update_marks(obtained_mark:, available_marks:)
+    # Note: Assumes data has been validated.
+    self.obtained_mark = [self.obtained_mark, obtained_mark].max
+    self.available_marks = [self.available_marks, available_marks].max
+  end
 end
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -1,4 +1,33 @@
 require "test_helper"
 
 class ResultTest < ActiveSupport::TestCase
+  describe '.new' do
+    it 'defaults scores to zero' do
+      result = Result.new
+
+      assert_equal result.obtained_mark, 0
+      assert_equal result.available_marks, 0
+
+      result = Result.new(obtained_mark: 7, available_marks: 9)
+
+      assert_equal result.obtained_mark, 7
+      assert_equal result.available_marks, 9
+    end
+  end
+
+  describe '#update_marks' do
+    it 'updates marks with if and only if new mark is greater than existing' do
+      result = Result.new(obtained_mark: 6, available_marks: 7)
+
+      result.update_marks(obtained_mark: 7, available_marks: 9)
+
+      assert_equal result.obtained_mark, 7
+      assert_equal result.available_marks, 9
+
+      result.update_marks(obtained_mark: 3, available_marks: 5)
+
+      assert_equal result.obtained_mark, 7
+      assert_equal result.available_marks, 9
+    end
+  end
 end
